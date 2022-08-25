8d6c2ac1481097517939baa2c2c2c9b932e8b028
Adam Davies <adam.davies@onehundredknights.co.uk>
2022-08-25 10:02:43 +0100

    wip

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,28 +1,6 @@
 import "./bootstrap";
 import Alpine from "alpinejs";
-// import Quill from "quill";
 
 window.Alpine = Alpine;
 
 Alpine.start();
-
-// var options = {
-//     debug: "info",
-//     modules: {
-//         toolbar: [
-//             [{ header: [2, false] }],
-//             ["bold", "italic", "underline"],
-//             [{ list: "ordered" }, { list: "bullet" }],
-//         ],
-//     },
-//     placeholder: "Compose an epic...",
-//     theme: "snow",
-// };
-
-// var editor = new Quill("#job_description", options);
-
-// document.addEventListener("keydown", (event) => {
-//     if (event.code == "Backquote") {
-//         console.log(editor.getContents());
-//     }
-// });
